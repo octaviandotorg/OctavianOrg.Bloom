9cdf5ab564bb4996a3f7cf9abc6d6024d86a848f
PF <octaviandotorg@gmail.com>
2024-10-29 20:44:16 -0700

    Added base class for bloom filter and created concurrent version. Modified unit tests.

---
+++ OctavianOrg.Bloom/ConcurrentBloomFilter.cs
@@ -0,0 +1,74 @@
+﻿
+
+using System;
+using System.Threading;
+
+namespace OctavianOrg.Bloom
+{
+    public class ConcurrentBloomFilter : BasicBloomFilter
+    {
+        private object[,] _locks;
+
+        public ConcurrentBloomFilter(BloomFilterParameters parameters, HashProvider hashProvider, int concurrencyLevel) : base(parameters, hashProvider)
+        {
+            if (concurrencyLevel <= 0)
+            {
+                if (concurrencyLevel != -1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(concurrencyLevel), "Concurrency level must be greater than 0 or -1 for default.");
+                }
+
+                concurrencyLevel = Environment.ProcessorCount;
+            }
+
+            ConcurrencyLevel = concurrencyLevel;
+
+            _locks = new object[Parameters.HashFunctionCount, concurrencyLevel];
+            
+            for (int i = 0; i < _locks.GetLength(0); ++i)
+            {
+                for (int j = 0; j < _locks.GetLength(1); ++j)
+                {
+                    _locks[i, j] = new object();
+                }
+            }
+        }
+
+        public int ConcurrencyLevel { get; private set; }
+
+        protected override void AddInternal(long[] hashValues)
+        {
+            for (int i = 0; i < Parameters.HashFunctionCount; ++i)
+            {
+                long bitIndex = hashValues[i] % Parameters.BitsPerHashFunction;
+                object chunkLock = _locks[i, bitIndex % ConcurrencyLevel];
+
+                lock (chunkLock)
+                {
+                    _bitSets[i].Set(bitIndex);
+                }
+            }
+
+            Interlocked.Increment(ref _count);
+        }
+
+        protected override bool ContainsInternal(long[] hashValues)
+        {
+            for (int i = 0; i < Parameters.HashFunctionCount; ++i)
+            {
+                long bitIndex = hashValues[i] % Parameters.BitsPerHashFunction;
+                object chunkLock = _locks[i, bitIndex % ConcurrencyLevel];
+
+                lock (chunkLock)
+                {
+                    if (_bitSets[i].IsSet(bitIndex) == false)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
